0baa3853f48de96f5edaa38a58680908ea17bbba
Peter Aigner <mastapetz@icloud.com>
2025-05-14 23:40:45 +0200

    create repo

---
+++ Lernen.py
@@ -0,0 +1,10 @@
+'''# Define a function that acts like a parrot
+# Which repeats the things you say back to you'''
+def parrot():
+    print("Gib etwas ein")
+    answer = input()
+    print("Deine eingabe war")
+    print(answer)
+
+'''# Call the parrot function'''
+parrot()
